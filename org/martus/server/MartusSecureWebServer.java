6641a4f7264c3f3f1fc6735126d62675855cc4fe
ivo <ivo@localhost>
2002-10-02 16:31:45 +0000

    refactored: organized imports

--- org/martus/server/MartusSecureWebServer.java
+++ org/martus/server/MartusSecureWebServer.java
@@ -1,30 +1,13 @@
 package org.martus.server;
 
 import java.io.IOException;
-import java.math.BigInteger;
 import java.net.ServerSocket;
-import java.security.InvalidKeyException;
-import java.security.KeyPair;
-import java.security.KeyPairGenerator;
-import java.security.KeyStore;
-import java.security.PrivateKey;
-import java.security.SignatureException;
-import java.security.cert.Certificate;
-import java.security.cert.X509Certificate;
-import java.security.interfaces.RSAPrivateCrtKey;
-import java.security.interfaces.RSAPublicKey;
-import java.util.Date;
-import java.util.Hashtable;
-import java.util.Vector;
 
-import javax.net.ssl.KeyManager;
-import javax.net.ssl.KeyManagerFactory;
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLServerSocketFactory;
+
 import org.apache.xmlrpc.WebServer;
-import org.bouncycastle.jce.X509Principal;
-import org.bouncycastle.jce.X509V1CertificateGenerator;
-import org.martus.common.*;
+import org.martus.common.MartusCrypto;
 
 public class MartusSecureWebServer extends WebServer
 {
