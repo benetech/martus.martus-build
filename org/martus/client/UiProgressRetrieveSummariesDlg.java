16c98eae8f1279fa5fb3f1452b6756741a74ac61
charles <charles@localhost>
2002-10-21 16:35:41 +0000

    reverted back to original

--- org/martus/client/UiProgressRetrieveSummariesDlg.java
+++ org/martus/client/UiProgressRetrieveSummariesDlg.java
@@ -1,26 +1,17 @@
 package org.martus.client;
 
 import java.awt.BorderLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
-import java.awt.event.WindowListener;
 
 import javax.swing.Box;
-import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
 
-import sun.awt.WindowClosingListener;
-
-public class UiProgressRetrieveSummariesDlg extends JDialog
+public class UiProgressRetrieveSummariesDlg extends JDialog 
 {
 	public UiProgressRetrieveSummariesDlg(UiMainWindow window, String tag)
 	{
 		super(window, window.getApp().getWindowTitle(tag), true);
 		setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
-		addWindowListener(new WindowEventHandler());
 		mainWindow = window;
 		bulletinCountMeter = new UiProgressMeter();
 		statusMessage = window.getApp().getFieldLabel(tag);
@@ -29,41 +20,12 @@ public class UiProgressRetrieveSummariesDlg extends JDialog
 		vBox.add(new JLabel("    "));
 		vBox.add(bulletinCountMeter);
 		vBox.add(new JLabel("    "));
-		cancel = new JButton(window.getApp().getButtonLabel("Cancel"));
-		cancel.addActionListener(new CancelHandler());
-		vBox.add(cancel);
 		getContentPane().add(vBox);
 		getContentPane().add(new JLabel("    "), BorderLayout.EAST);
 		getContentPane().add(new JLabel("    "), BorderLayout.WEST);
 		window.centerDlg(this);
-		
-	}
-
-	class WindowEventHandler extends WindowAdapter
-	{
-		public void windowClosing(WindowEvent event)
-		{
-			System.out.println("out");
-			RequestExit();
-		}
-	}
-
-	class CancelHandler implements ActionListener
-	{
-		public void actionPerformed(ActionEvent ae)
-		{
-			System.out.println(ae.getSource());
-			if(ae.getSource() == cancel)
-				RequestExit();
-		}
 	}
 	
-	private void RequestExit()
-	{
-		requestExit = true;
-		cancel.setEnabled(false);
-	}
-
 	public void beginRetrieve()
 	{
 		show();
@@ -74,11 +36,6 @@ public class UiProgressRetrieveSummariesDlg extends JDialog
 		dispose();	
 	}
 	
-	public boolean shouldExit()
-	{
-		return requestExit;	
-	}
-	
 	public void updateBulletinCountMeter(int currentValue, int maxValue)
 	{
 		bulletinCountMeter.updateProgressMeter(statusMessage, currentValue, maxValue);	
@@ -87,7 +44,4 @@ public class UiProgressRetrieveSummariesDlg extends JDialog
 	private UiProgressMeter bulletinCountMeter;
 	private UiMainWindow mainWindow;
 	private String statusMessage;
-	private JButton cancel;
-	private boolean requestExit;
-
 }
