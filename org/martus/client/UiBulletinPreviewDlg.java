03885b38fb9b42a0dac40fbff7e31d82b2a7e536
charles <charles@localhost>
2002-10-28 15:00:51 +0000

    preview Dlg update on bulletin summaries.

--- org/martus/client/UiBulletinPreviewDlg.java
+++ org/martus/client/UiBulletinPreviewDlg.java
@@ -1,16 +1,20 @@
 package org.martus.client;
 
+import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
+import javax.swing.JScrollPane;
+import javax.swing.JViewport;
 
 import org.martus.common.FieldDataPacket;
 
 public class UiBulletinPreviewDlg extends JDialog implements ActionListener
 {
+	
 	public UiBulletinPreviewDlg(UiMainWindow owner, FieldDataPacket fdp)
 	{
 		super(owner, owner.getApp().getWindowTitle("BulletinPreview"), true);
@@ -25,11 +29,18 @@ public class UiBulletinPreviewDlg extends JDialog implements ActionListener
 		view.disableEdits();
 		view.attachmentViewer.saveButton.setVisible(false);
 
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.getViewport().setScrollMode(JViewport.SIMPLE_SCROLL_MODE);
+		scrollPane.getViewport().add(view);
+		scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+		scrollPane.setMaximumSize(new Dimension(600, 400));
+		scrollPane.setPreferredSize(new Dimension(600, 400));
+
 		JButton ok = new JButton(owner.getApp().getButtonLabel("ok"));
 		ok.addActionListener(this);
 
 		getContentPane().add(new JLabel(" "), ParagraphLayout.NEW_PARAGRAPH);
-		getContentPane().add(view, ParagraphLayout.NEW_PARAGRAPH);
+		getContentPane().add(scrollPane, ParagraphLayout.NEW_PARAGRAPH);
 		getContentPane().add(new JLabel(" "), ParagraphLayout.NEW_PARAGRAPH);
 		getContentPane().add(ok, ParagraphLayout.NEW_PARAGRAPH);
 		getContentPane().add(new JLabel(" "), ParagraphLayout.NEW_PARAGRAPH);
