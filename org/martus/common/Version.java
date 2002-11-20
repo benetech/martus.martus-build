21e42b0b15edc89173acac015d3d105ab05cad18
ivo <ivo@localhost>
2002-11-20 02:49:32 +0000

    updated to output human readable version

--- org/martus/common/Version.java
+++ org/martus/common/Version.java
@@ -5,6 +5,14 @@ public class Version
 	public static void main(String[] args) 
 	{
 		MartusUtilities utilities = new MartusUtilities();
-		System.out.println(utilities.getVersionDate(utilities.getClass()));
+		String date = utilities.getVersionDate(utilities.getClass());
+		System.out.println(formatDateVersion(date));
+	}
+	
+	static String formatDateVersion(String dateVersion)
+	{
+		if(dateVersion.length() != 8)
+			return dateVersion;
+		return dateVersion.substring(0,4) + "-" + dateVersion.substring(4,6) + "-" + dateVersion.substring(6);
 	}
 }
