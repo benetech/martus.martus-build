293e3405e3d95c67af84014a8342a403be5cb6f9
uid505 <uid505@localhost>
2003-01-09 23:51:28 +0000

    Update version to 1.0.1

--- org/martus/client/UiConstants.java
+++ org/martus/client/UiConstants.java
@@ -31,5 +31,5 @@ public class UiConstants
 	public static final int defaultFontSize = 13;
 	public static final int textFieldColumns = 40;
 	
-	public static final String versionLabel = "1.0";
+	public static final String versionLabel = "1.0.1";
 }
