b096ac69943b0675ec5580d8c704a9b22e68a60d
ivo <ivo@localhost>
2003-01-24 18:53:29 +0000

    def 597. tool to export the keypair public key

---
+++ org/martus/server/tools/ExportPublicKey.java
@@ -0,0 +1,97 @@
+package org.martus.server.tools;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import org.martus.common.MartusCrypto;
+import org.martus.common.MartusUtilities;
+import org.martus.common.Base64.InvalidBase64Exception;
+import org.martus.common.MartusCrypto.MartusSignatureException;
+import org.martus.server.forclients.MartusServerUtilities;
+
+public class ExportPublicKey
+{
+	public static void main(String[] args)
+	{
+		File keypair = null;
+		File outputfile = null;
+		boolean prompt = true;
+		for (int i = 0; i < args.length; i++)
+		{
+			if(args[i].startsWith("--no-prompt"))
+			{
+				prompt = false;
+			}
+			
+			if(args[i].startsWith("--keypair"))
+			{
+				keypair = new File(args[i].substring(args[i].indexOf("=")+1));
+			}
+			
+			if(args[i].startsWith("--file"))
+			{
+				outputfile = new File(args[i].substring(args[i].indexOf("=")+1));
+			}
+		}
+		
+		if(keypair == null || outputfile == null)
+		{
+			System.err.println("Incorrect arguments: ExportPublicKey [--no-prompt] --keypair=keypair.dat --file=pubkey.dat");
+			System.exit(2);
+		}
+		
+		if(!keypair.exists())
+		{
+			System.err.println("Unable to find keypair");
+			System.exit(3);
+		}
+		
+		if(prompt)
+		{
+			System.out.print("Enter server passphrase:");
+			System.out.flush();
+		}
+		
+		MartusCrypto security = null;
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		try
+		{
+			String passphrase = reader.readLine();
+			security = MartusServerUtilities.loadCurrentMartusSecurity(keypair, passphrase);
+		}
+		catch(Exception e)
+		{
+			System.err.println("ExportPublicKey.main: " + e);
+			System.exit(3);
+		}
+		
+		try
+		{
+			MartusUtilities.exportPublicKey(security, outputfile);
+		}
+		catch (MartusSignatureException e)
+		{
+			System.err.println("ExportPublicKey.main: " + e);
+			System.exit(3);
+		}
+		catch (InvalidBase64Exception e)
+		{
+			System.err.println("ExportPublicKey.main: " + e);
+			System.exit(3);
+		}
+		catch (IOException e)
+		{
+			System.err.println("ExportPublicKey.main: " + e);
+			System.exit(3);
+		}
+
+		if(prompt)
+		{
+			System.out.print("Public key exported to file " + outputfile.getAbsolutePath());
+			System.out.flush();
+		}
+		System.exit(0);
+	}
+}
