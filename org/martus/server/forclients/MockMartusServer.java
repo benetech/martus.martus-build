c1147ef59981fba491681015726b4cd6050e1acd
uid505 <uid505@localhost>
2003-05-20 17:44:04 +0000

    Minor change to Exception declaration

--- org/martus/server/forclients/MockMartusServer.java
+++ org/martus/server/forclients/MockMartusServer.java
@@ -34,7 +34,7 @@ public class MockMartusServer extends MartusServer
 		database = databaseToUse;
 	}
 	
-	public void initialize() throws IOException
+	public void initialize() throws Exception
 	{
 		try
 		{
