a24119504409e77c605463178e5502beeab7c127
uid505 <uid505@localhost>
2003-06-18 19:27:13 +0000

    Revert earlier change. Now just print internal version (not build date/number)

--- org/martus/server/tools/Version.java
+++ org/martus/server/tools/Version.java
@@ -1,14 +1,11 @@
 package org.martus.server.tools;
 
-import org.martus.common.MartusUtilities;
 import org.martus.server.core.ServerConstants;
 
 public class Version
 {
 	public static void main(String[] args)
 	{
-		String version = ServerConstants.version;
-		String build = MartusUtilities.getVersionDate();
-		System.out.println(version + " " + build);
+		System.out.println(ServerConstants.internalVersionForScriptCompatibility);
 	}
 }
