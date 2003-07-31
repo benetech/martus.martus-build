94b8bb2aecdb9db95c0d9ec4d7768911f87189de
uid505 <uid505@localhost>
2003-07-31 00:25:59 +0000

    Pull signin logic from dialog into panel so the dialog can be tabbed.

--- org/martus/client/swingui/dialogs/UiSigninDlg.java
+++ org/martus/client/swingui/dialogs/UiSigninDlg.java
@@ -26,34 +26,25 @@ Boston, MA 02111-1307, USA.
 
 package org.martus.client.swingui.dialogs;
 
-import java.awt.Color;
-import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.IOException;
 
-import javax.swing.AbstractAction;
+import javax.swing.Box;
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JPasswordField;
-import javax.swing.JTextField;
-import javax.swing.border.LineBorder;
+import javax.swing.JTabbedPane;
 
 import org.martus.client.swingui.UiConstants;
 import org.martus.client.swingui.UiLocalization;
 import org.martus.client.swingui.UiMainWindow;
-import org.martus.client.swingui.UiVirtualKeyboard;
-import org.martus.client.swingui.VirtualKeyboardHandler;
-import org.martus.swing.ParagraphLayout;
-import org.martus.swing.UiWrappedTextArea;
+import org.martus.client.swingui.UiSigninPanel;
 import org.martus.swing.Utilities;
 
 
 
-public class UiSigninDlg extends JDialog implements VirtualKeyboardHandler
+public class UiSigninDlg extends JDialog
 {
 	public UiSigninDlg(UiMainWindow window, JFrame owner, int mode)
 	{
@@ -63,11 +54,11 @@ public class UiSigninDlg extends JDialog implements VirtualKeyboardHandler
 	public UiSigninDlg(UiMainWindow window, JFrame owner, int mode, String username)
 	{
 		super(owner, true);
-		Initalize(window, owner, mode, username);
+		initalize(window, owner, mode, username);
 	}
 
 
-	public void Initalize(UiMainWindow window, JFrame owner, int mode, String username)
+	public void initalize(UiMainWindow window, JFrame owner, int mode, String username)
 	{
 		mainWindow = window;
 		
@@ -75,79 +66,47 @@ public class UiSigninDlg extends JDialog implements VirtualKeyboardHandler
 
 		String title = getTextForTitle(localization, mode);
 		setTitle(title);
+		
+		JTabbedPane tabbedPane = new JTabbedPane();
+		contentPane = new UiSigninPanel(this, mode, username);
+		tabbedPane.add(contentPane);
+		tabbedPane.setTitleAt(0, "Sign In");  
 
-		getContentPane().setLayout(new ParagraphLayout());
 		ok = new JButton(localization.getButtonLabel("ok"));
 		ok.addActionListener(new OkHandler());
 		JButton cancel = new JButton(localization.getButtonLabel("cancel"));
 		cancel.addActionListener(new CancelHandler());
-		if(mode == TIMED_OUT)
-		{
-			getContentPane().add(new JLabel(""), ParagraphLayout.NEW_PARAGRAPH);
-			JLabel timedOutNote1 = new JLabel(localization.getFieldLabel("timedout1"));
-			getContentPane().add(timedOutNote1);
-			if(window.isModifyingBulletin())
-			{
-				getContentPane().add(new JLabel(""), ParagraphLayout.NEW_PARAGRAPH);
-				JLabel timedOutNote2 = new JLabel(localization.getFieldLabel("timedout2"));
-				getContentPane().add(timedOutNote2);
-			}
-		}
-		else if(mode == SECURITY_VALIDATE)
-		{
-			getContentPane().add(new JLabel(""), ParagraphLayout.NEW_PARAGRAPH);
-			JLabel securityServerConfigValidate = new JLabel(localization.getFieldLabel("securityServerConfigValidate"));
-			getContentPane().add(securityServerConfigValidate);
-		}
-		else if(mode == RETYPE_USERNAME_PASSWORD)
-		{
-			getContentPane().add(new JLabel(""), ParagraphLayout.NEW_PARAGRAPH);
-			JLabel retypeUserNamePassword = new JLabel(localization.getFieldLabel("RetypeUserNameAndPassword"));
-			getContentPane().add(retypeUserNamePassword);
-		}
-		else if(mode == CREATE_NEW)
-		{
-			getContentPane().add(new JLabel(""), ParagraphLayout.NEW_PARAGRAPH);
-			JLabel createNewUserNamePassword = new JLabel(localization.getFieldLabel("CreateNewUserNamePassword"));
-			getContentPane().add(createNewUserNamePassword);
-
-			getContentPane().add(new JLabel(""), ParagraphLayout.NEW_PARAGRAPH);
-			UiWrappedTextArea helpOnCreatingPassword = new UiWrappedTextArea(localization.getFieldLabel("HelpOnCreatingNewPassword"));
-			getContentPane().add(helpOnCreatingPassword);
-
-		}
-
-		userNameDescription = new JLabel("");
-		passwordDescription = new JLabel("");
-
-		getContentPane().add(new JLabel(localization.getFieldLabel("username")), ParagraphLayout.NEW_PARAGRAPH);
-		nameField = new JTextField(20);
-		nameField.setText(username);
-		getContentPane().add(userNameDescription);
-		getContentPane().add(nameField);
-
-		getContentPane().add(new JLabel(localization.getFieldLabel("password")), ParagraphLayout.NEW_PARAGRAPH);
-		passwordField = new JPasswordField(20);
-
-		switchToNormalKeyboard = new JButton(localization.getButtonLabel("VirtualKeyboardSwitchToNormal"));
-		switchToNormalKeyboard.addActionListener(new switchKeyboardHandler());
-		passwordArea = new JPanel();
-		getContentPane().add(passwordArea);
-		keyboard = new UiVirtualKeyboard(localization, this);
-		UpdatePasswordArea();
-
-		getContentPane().add(new JLabel(""), ParagraphLayout.NEW_PARAGRAPH);
-		getContentPane().add(ok);
-		getContentPane().add(cancel);
+		Box buttonBox = Box.createHorizontalBox();
+		buttonBox.add(ok);
+		buttonBox.add(cancel);
+		
+		Box vbox = Box.createVerticalBox();
+		vbox.add(tabbedPane);
+		vbox.add(new JLabel(" "));
+		vbox.add(buttonBox);
+		vbox.add(new JLabel(" "));
+		getContentPane().add(vbox);
 
 		getRootPane().setDefaultButton(ok);
-		if(username.length() > 0)
-			passwordField.requestFocus();
 		Utilities.centerDlg(this);
 		setResizable(true);
 		show();
 	}
 
+/*
+ We don't know if this is being called. Maybe it should be deleted.
+ 
+	public void updateUI()
+	{
+		getRootPane().setDefaultButton(ok);
+		super.updateUI();
+	}
+*/
+	public UiMainWindow getMainWindow()
+	{
+		return mainWindow;
+	}
+	
 	public static String getTextForTitle(UiLocalization localization, int mode)
 	{
 		String versionInfo = UiConstants.programName;
@@ -178,122 +137,22 @@ public class UiSigninDlg extends JDialog implements VirtualKeyboardHandler
 
 	public String getName()
 	{
-		return name;
+		return contentPane.getName();
 	}
 
 	public String getPassword()
 	{
-		return password;
+		return contentPane.getPassword();
 	}
-
-	public class switchKeyboardHandler extends AbstractAction
+	
+	public void sizeHasChanged()
 	{
-		public void actionPerformed(ActionEvent e)
-		{
-			SwitchKeyboards();
-		}
+		Utilities.centerDlg(this);
 	}
-	public void setPassword(String virtualPassword)
+	
+	public void virtualPasswordHasChanged()
 	{
-		passwordField.setText(virtualPassword);
-		passwordField.updateUI();
 		ok.requestFocus();
-	}
-
-	public void SwitchKeyboards()
-	{
-		boolean viewingVirtualKeyboard = mainWindow.isCurrentDefaultKeyboardVirtual();
-		if(viewingVirtualKeyboard)
-		{
-			if(!mainWindow.confirmDlg(null, "WarningSwitchToNormalKeyboard"))
-				return;
-		}
-		mainWindow.setCurrentDefaultKeyboardVirtual(!viewingVirtualKeyboard);
-		try
-		{
-			mainWindow.saveCurrentUiState();
-		}
-		catch(IOException e)
-		{
-			System.out.println("UiSigninDialog SwitchKeyboards :" + e);
-		}
-
-		UpdatePasswordArea();
-	}
-
-	public void UpdatePasswordArea()
-	{
-		boolean viewingVirtualKeyboard = mainWindow.isCurrentDefaultKeyboardVirtual();
-		if(viewingVirtualKeyboard)
-			displayPasswordAreaUsingVirtualKeyboard();
-		else
-			displayPasswordAreaUsingNormalKeyboard();
-	}
-
-	public void addKeyboard(JPanel keyboard)
-	{
-		virtualKeyboardPanel = keyboard;
-	}
-
-	public void displayPasswordAreaUsingVirtualKeyboard()
-	{
-		UiLocalization localization = mainWindow.getLocalization();
-
-		passwordArea.removeAll();
-		userNameDescription.setText(localization.getFieldLabel("VirtualUserNameDescription"));
-		passwordDescription.setText(localization.getFieldLabel("VirtualPasswordDescription"));
-
-		passwordArea.setLayout(new ParagraphLayout());
-		passwordArea.setBorder(new LineBorder(Color.black, 2));
-		passwordArea.add(new JLabel(""));
-		passwordArea.add(passwordDescription);
-		passwordField.setEditable(false);
-		passwordArea.add(passwordField);
-
-		passwordArea.add(new JLabel(""), ParagraphLayout.NEW_PARAGRAPH);
-		passwordArea.add(virtualKeyboardPanel);
-
-		passwordArea.add(new JLabel(""), ParagraphLayout.NEW_PARAGRAPH);
-		switchToNormalKeyboard.setText(localization.getButtonLabel("VirtualKeyboardSwitchToNormal"));
-		passwordArea.add(switchToNormalKeyboard);
-		updateUI();
-		Utilities.centerDlg(this);
-	}
-
-	public void displayPasswordAreaUsingNormalKeyboard()
-	{
-		UiLocalization localization = mainWindow.getLocalization();
-
-		passwordArea.removeAll();
-		passwordArea.updateUI();
-		userNameDescription.setText("");
-		passwordDescription.setText("");
-		passwordArea.setLayout(new ParagraphLayout());
-		passwordArea.setBorder(new LineBorder(Color.black, 2));
-
-		passwordField.setEditable(true);
-		passwordArea.add(new JLabel(""), ParagraphLayout.NEW_PARAGRAPH);
-		passwordArea.add(passwordField);
-
-		passwordArea.add(new JLabel(""), ParagraphLayout.NEW_PARAGRAPH);
-		JLabel warningNormalKeyboard = new JLabel(localization.getFieldLabel("NormalKeyboardMsg1"));
-		warningNormalKeyboard.setFont(warningNormalKeyboard.getFont().deriveFont(Font.BOLD));
-		passwordArea.add(warningNormalKeyboard);
-		passwordArea.add(new JLabel(""), ParagraphLayout.NEW_PARAGRAPH);
-		passwordArea.add(new JLabel(localization.getFieldLabel("NormalKeyboardMsg2")));
-
-		passwordArea.add(new JLabel(""), ParagraphLayout.NEW_PARAGRAPH);
-		switchToNormalKeyboard.setText(localization.getButtonLabel("VirtualKeyboardSwitchToVirtual"));
-		passwordArea.add(switchToNormalKeyboard);
-		updateUI();
-		Utilities.centerDlg(this);
-	}
-	public void updateUI()
-	{
-		passwordArea.updateUI();
-		userNameDescription.updateUI();
-		getRootPane().setDefaultButton(ok);
-		nameField.requestFocus();
 	}
 
 	class OkHandler implements ActionListener
@@ -301,8 +160,6 @@ public class UiSigninDlg extends JDialog implements VirtualKeyboardHandler
 		public void actionPerformed(ActionEvent ae)
 		{
 			result = true;
-			name = nameField.getText();
-			password = new String(passwordField.getPassword());
 			dispose();
 		}
 	}
@@ -315,18 +172,9 @@ public class UiSigninDlg extends JDialog implements VirtualKeyboardHandler
 		}
 	}
 
+	UiSigninPanel contentPane;
 	private UiMainWindow mainWindow;
 	private boolean result;
-	private String name;
-	private String password;
-	private UiVirtualKeyboard keyboard;
-	private JPanel passwordArea;
-	private JTextField nameField;
-	private JPasswordField passwordField;
-	private JPanel virtualKeyboardPanel;
-	private JLabel userNameDescription;
-	private JLabel passwordDescription;
-	private JButton switchToNormalKeyboard;
 	private JButton ok;
 
 	public final static int INITIAL = 1;
