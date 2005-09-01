85d0881a9837c8ace5431d00bdb8637115048925
ivo <ivo@localhost>
2005-09-01 22:44:14 +0000

    v 2005-09-01 build 1660

--- MartusBuild.sh
+++ MartusBuild.sh
@@ -77,7 +77,7 @@ setCvsEnvVars()
 	MARTUSINSTALLERPROJECT=$CVS_HOME/binary-martus/Installer
 	MARTUSNSISPROJECTDIR=$MARTUSINSTALLERPROJECT/Win32_NSIS
 	MARTUSBUILDFILES=$MARTUSINSTALLERPROJECT/BuildFiles
-
+	
 	RELEASE_DIR=/cygdrive/c/SharedDocs/MatusReleases
 	PREVIOUS_RELEASE_DIR=/cygdrive/c/SharedDocs/Prev.MatusReleases
 
@@ -203,7 +203,7 @@ downloadMartusInstallerFromCvsAndSetup()
 	echo
 	echo "CD Build necessary, downloading installer from CVS...";
 	cvs checkout binary-martus/Installer/ 2>&1 || error "cvs returned $?"
-	
+		
 	copyThirdPartyJarToCDBuild
 	copyThirdPartySourceToCDBuild
 	copyThirdPartyLicenseToCDBuild
@@ -310,10 +310,10 @@ setupBuildEnvironment()
 	else
 		BUILD_NUMBER=1
 	fi
-
+	
 	echo
 	echo "Build is v $CURRENT_VERSION, b $BUILD_NUMBER, date $BUILD_DATE"
-	
+		
 	BUILD_OUTPUT_DIR=$CVS_HOME/martus/dist
 	BUILD_VERNUM_TAG=$BUILD_DATE.$BUILD_NUMBER
 	
@@ -361,7 +361,7 @@ startAntBuild()
 		echo "Exiting..."
 		exit 1
 	fi
-
+	
 	if [ $cvs_tag = 1 ]; then
 		if [ ! -f "$MARTUS_JAR_FILE.sha" ]; then
 			echo "BUILD FAILED!! Missing sha. Exit status $status"
