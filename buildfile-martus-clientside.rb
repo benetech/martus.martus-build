00dbadc41383602b99d108a68fcd21010574b2aa
kevins <kevins@localhost>
2009-12-29 18:10:58 +0000

    Fix some build errors in martus-clientside buildfile

--- buildfile-martus-clientside.rb
+++ buildfile-martus-clientside.rb
@@ -7,8 +7,11 @@ define name, :layout=>create_layout_with_source_as_source(name) do
 	compile.options.target = '1.5'
 	compile.with(
 		'junit:junit:jar:3.8.2',
+		'org.martus:martus-utils:jar:1',
 		'org.martus:martus-common:jar:1',
-		'org.martus:martus-swing:jar:1'
+		'org.martus:martus-swing:jar:1',
+		'com.jhlabs:layouts:jar:2006-08-10',
+		'xmlrpc:xmlrpc:jar:1.2-b1'
 	)
   
 	package :jar
