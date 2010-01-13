af4b6674c54b73da7a4bdab6fa4f2b3687cf2899
kevins <kevins@localhost>
2010-01-13 20:39:53 +0000

    Add third party jars to nsis upgrade build script

--- buildfile-martus-client-nsis-upgrade.rb
+++ buildfile-martus-client-nsis-upgrade.rb
@@ -4,6 +4,16 @@ define name, :layout=>create_layout_with_source_as_source(name) do
 	project.group = 'org.martus'
 	project.version = '1'
 
+	package(:zip).include(artifact(ICU4J_SPEC), :path=>'LibExt')
+	package(:zip).include(artifact(INFINITEMONKEY_DLL_SPEC), :path=>'LibExt')
+	package(:zip).include(artifact(INFINITEMONKEY_JAR_SPEC), :path=>'LibExt')
+	package(:zip).include(artifact(BCPROV_SPEC), :path=>'LibExt')
+	package(:zip).include(artifact(PERSIANCALENDAR_SPEC), :path=>'LibExt')
+	package(:zip).include(artifact(XMLRPC_SPEC), :path=>'LibExt')
+	package(:zip).include(artifact(JUNIT_SPEC), :path=>'LibExt')
+	package(:zip).include(artifact(VELOCITY_SPEC), :path=>'LibExt')
+	package(:zip).include(artifact(VELOCITY_DEP_SPEC), :path=>'LibExt')
+	package(:zip).include(artifact(LAYOUTS_SPEC), :path=>'LibExt')
 	package(:zip).include(project('martus-client').package(:jar))
 	#TODO: need to zip up everything else that goes into the nsis upgrade installer
 
