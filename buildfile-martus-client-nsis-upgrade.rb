4ff4e8bebdc6925747bcae2a312426a29e3456de
kevins <kevins@localhost>
2010-01-15 19:42:18 +0000

    build script now attempts to run makensis (currently gets errors due to missing files)

--- buildfile-martus-client-nsis-upgrade.rb
+++ buildfile-martus-client-nsis-upgrade.rb
@@ -4,46 +4,52 @@ define name, :layout=>create_layout_with_source_as_source(name) do
 	project.group = 'org.martus'
 	project.version = '1'
 
-	package(:zip).include(_('BuildFiles/*.txt'))
+	package(:zip).include(_('BuildFiles/*.txt'), :path=>'BuildFiles')
 
 	# Need to include SourceFiles
-	package(:zip).include(_('BuildFiles/Windows/Winsock95'), :path=>'Verifier')
-	package(:zip).include(_('martus-jar-verifier/*.txt'), :path=>'Verifier')
-	package(:zip).include(_('martus-jar-verifier/*.bat'), :path=>'Verifier')
-	package(:zip).include(_('martus-jar-verifier/source'), :path=>'Verifier')
+	package(:zip).include(_('BuildFiles/Windows/Winsock95'), :path=>'BuildFiles/Verifier')
+	package(:zip).include(_('martus-jar-verifier/*.txt'), :path=>'BuildFiles/Verifier')
+	package(:zip).include(_('martus-jar-verifier/*.bat'), :path=>'BuildFiles/Verifier')
+	package(:zip).include(_('martus-jar-verifier/source'), :path=>'BuildFiles/Verifier')
 	# Need to include MartusWin32SetupLauncher?
-	package(:zip).include(_('BuildFiles/ProgramFiles'))
-	package(:zip).include(_('BuildFiles/SampleDir'))
+	package(:zip).include(_('BuildFiles/ProgramFiles'), :path=>'BuildFiles')
+	package(:zip).include(_('BuildFiles/SampleDir'), :path=>'BuildFiles')
 	# Need to include MartusSetupLauncher?
 
 	#TODO: Should we really include the MSPA zip?
-	package(:zip).include(_('martus-mspa/target/MartusMSPA.zip'), :path=>'Jars')
-	package(:zip).include(project('martus-bc-jce').package(:jar), :path=>'Jars')
-	package(:zip).include(artifact(RHINO_SPEC), :path=>'Jars')
-	package(:zip).include(artifact(LAYOUTS_SPEC), :path=>'Jars')
-	package(:zip).include(artifact(BCPROV_SPEC), :path=>'Jars')
-	package(:zip).include(artifact(JUNIT_SPEC), :path=>'Jars')
-	package(:zip).include(artifact(ICU4J_SPEC), :path=>'Jars')
-	package(:zip).include(artifact(PERSIANCALENDAR_SPEC), :path=>'Jars')
-	package(:zip).include(artifact(VELOCITY_SPEC), :path=>'Jars')
-	package(:zip).include(artifact(VELOCITY_DEP_SPEC), :path=>'Jars')
-	package(:zip).include(artifact(INFINITEMONKEY_JAR_SPEC), :path=>'Jars')
-	package(:zip).include(artifact(XMLRPC_SPEC), :path=>'Jars')
-	package(:zip).include(_('BuildFiles/JavaRedistributables/Win32'), :path=>'Java redist/Win32')
-	package(:zip).include(_('BuildFiles/Documents'))
+	package(:zip).include(_('martus-mspa/target/MartusMSPA.zip'), :path=>'BuildFiles/Jars')
+	package(:zip).include(project('martus-bc-jce').package(:jar), :path=>'BuildFiles/Jars')
+	package(:zip).include(artifact(RHINO_SPEC), :path=>'BuildFiles/Jars')
+	package(:zip).include(artifact(LAYOUTS_SPEC), :path=>'BuildFiles/Jars')
+	package(:zip).include(artifact(BCPROV_SPEC), :path=>'BuildFiles/Jars')
+	package(:zip).include(artifact(JUNIT_SPEC), :path=>'BuildFiles/Jars')
+	package(:zip).include(artifact(ICU4J_SPEC), :path=>'BuildFiles/Jars')
+	package(:zip).include(artifact(PERSIANCALENDAR_SPEC), :path=>'BuildFiles/Jars')
+	package(:zip).include(artifact(VELOCITY_SPEC), :path=>'BuildFiles/Jars')
+	package(:zip).include(artifact(VELOCITY_DEP_SPEC), :path=>'BuildFiles/Jars')
+	package(:zip).include(artifact(INFINITEMONKEY_JAR_SPEC), :path=>'BuildFiles/Jars')
+	package(:zip).include(artifact(XMLRPC_SPEC), :path=>'BuildFiles/Jars')
+	package(:zip).include(_('BuildFiles/JavaRedistributables/Win32'), :path=>'BuildFiles/Java redist/Win32')
+	package(:zip).include(_('BuildFiles/Documents'), :path=>'BuildFiles')
 	# Need to include Documents/Licenses
 
-	package(:zip).include(artifact(INFINITEMONKEY_DLL_SPEC), :path=>'ProgramFiles')
-	package(:zip).include(project('martus-client').package(:jar), :path=>'ProgramFiles', :as=>'martus.jar')
+	package(:zip).include(artifact(INFINITEMONKEY_DLL_SPEC), :path=>'BuildFiles/ProgramFiles')
+	package(:zip).include(project('martus-client').package(:jar), :path=>'BuildFiles/ProgramFiles', :as=>'martus.jar')
 
-	package(:zip).include(_('BuildFiles/Windows/Win32_NSIS'), :path=>'Installer/NSIS Scripts')
+	package(:zip).include(_('BuildFiles/Windows/Win32_NSIS'))
 
 
 	update_packaged_zip(package(:zip)) do | filespec |
-		dest_dir = File.join(File.dirname(filespec), 'temp')
+		dest_dir = File.join(File.dirname(filespec), 'Installer')
 		Dir.mkdir(dest_dir)
 		unzip_file(filespec, dest_dir)
-		#TODO: Need to run makensis in that directory
+		
+		error_output = `makensis -V2 #{_(:target, '/Installer/Win32_NSIS/NSIS_Martus_Upgrade.nsi')}`
+		status = $?
+		if error_output.size > 0
+			raise "Error running makensis #{status.exitstatus}: #{error_output.split("\n").join("\n  ")}"
+		end
+		puts 'Finished makensis'
 	end
 end
 
