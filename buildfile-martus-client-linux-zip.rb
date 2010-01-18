acef7af0d7bdc9814909b06aa3135dfdca122fdb
kevins <kevins@localhost>
2010-01-18 18:29:39 +0000

    Add client source to linux zip in buildfile

--- buildfile-martus-client-linux-zip.rb
+++ buildfile-martus-client-linux-zip.rb
@@ -18,8 +18,8 @@ define name, :layout=>create_layout_with_source_as_source(name) do
 	package(:zip).include(artifact(INFINITEMONKEY_JAR_SPEC), :path=>'LibExt')
 	package(:zip).include(artifact(JUNIT_SPEC), :path=>'LibExt')
 	package(:zip).include(artifact(XMLRPC_SPEC), :path=>'LibExt')
+	package(:zip).include(project('martus-client').package(:source))
 #TODO: Add files to Mac/Linux zip
-#	package(:zip).include(client source zip)
 #	package(:zip).include(docs)
 #	package(:zip).include(licenses)
 	
