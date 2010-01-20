26b1f229e96ff5ab24c21a28956c3632b6fee46f
kevins <kevins@localhost>
2010-01-20 00:38:53 +0000

    Include user docs in iso build

--- buildfile-martus-client-iso.rb
+++ buildfile-martus-client-iso.rb
@@ -23,15 +23,14 @@ define name, :layout=>create_layout_with_source_as_source(name) do
 	package(:zip).include(_('BuildFiles', 'Documents', 'martus_user_guide.pdf'), :path=>'BuildFiles/Martus/Docs')
 	package(:zip).include(_('BuildFiles', 'Documents', 'quickstartguide.pdf'), :path=>'BuildFiles/Martus/Docs')
 
-	# TODO: Include documentation for all supported languages in CD ISO	
-#	for martus_lang in $MARTUS_LANGUAGES
-#		do
-#		echo -e "\tcopying docs for language: ${martus_lang}"
-#		cp -v $MARTUSBUILDFILES/Documents/README_${martus_lang}.txt $CD_IMAGE_DIR || message "ERROR: Unable to copy $MARTUSBUILDFILES/Documents/README_${martus_lang}.txt"
-#		cp -v $MARTUSBUILDFILES/Documents/martus_user_guide_${martus_lang}.pdf $CD_IMAGE_DIR/Martus/Docs || message "ERROR: Unable to copy $MARTUSBUILDFILES/Documents/martus_user_guide_${martus_lang}.pdf"
-#		cp -v $MARTUSBUILDFILES/Documents/quickstartguide_${martus_lang}.pdf $CD_IMAGE_DIR/Martus/Docs || message "ERROR: Unable to copy $MARTUSBUILDFILES/Documents/quickstartguide_${martus_lang}.pdf"
-#		cp -v $MARTUSBUILDFILES/Verify/readme_verify_${martus_lang}.txt $CD_IMAGE_DIR/verify/ || message "ERROR: Unable to copy $CD_IMAGE_DIR/readme_verify_${martus_lang}.txt"
-#	done
+	# TODO: Ask Anna about missing 'fa' user documents
+	martus_languages = ['es','ru','ar','fr','th','ne']
+	martus_languages.each do | language |
+		package(:zip).include(_('BuildFiles', 'Documents', "README_#{language}.txt"), :path=>'BuildFiles')
+		package(:zip).include(_('BuildFiles', 'Documents', "martus_user_guide_#{language}.pdf"), :path=>'BuildFiles/Martus/Docs')
+		package(:zip).include(_('BuildFiles', 'Documents', "quickstartguide_#{language}.pdf"), :path=>'BuildFiles/Martus/Docs')
+		package(:zip).include(project('martus-jar-verifier').path_to(:root, "readme_verify_#{language}.txt"), :path=>'BuildFiles')
+	end
 
 	package(:zip).include(_('BuildFiles', 'Documents', 'LinuxJavaInstall.txt'), :path=>'BuildFiles/Martus/Docs')
 
