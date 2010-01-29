454e2cef7332d884014b7cca04a026e3610f6288
kevins <kevins@localhost>
2010-01-29 18:58:52 +0000

    Include mspa user guide in zip

--- buildfile-martus-mspa-client-zip.rb
+++ buildfile-martus-mspa-client-zip.rb
@@ -16,8 +16,7 @@ define name, :layout=>create_layout_with_source_as_source(name) do
 	package(:zip).include(project('martus-common').package(:jar))
 	package(:zip).include(project('martus-bc-jce').package(:jar))
 	package(:zip).include(project('martus-mspa').package(:jar))
-	#TODO: I can't find binary-martus/Installer/mspa_user_guide.pdf
-#	package(:zip).include(mspa client user guide)
+	package(:zip).include(_('BuildFiles', 'Documents', 'mspa_user_guide.pdf'))
 	#TODO: Should mspa zip include all source code too?
-	
+	#TODO: Should mspa zip include license files?
 end
