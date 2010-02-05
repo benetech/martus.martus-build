7738e4686f5a3a7c3ea9d45ed1eb91eff06b10c0
kevins <kevins@localhost>
2010-02-05 11:51:07 +0000

    Change linux zip build script to use build target instead of package

--- buildfile-martus-client-linux-zip.rb
+++ buildfile-martus-client-linux-zip.rb
@@ -5,23 +5,23 @@ define name, :layout=>create_layout_with_source_as_source(name) do
 	project.version = '1'
 	
 	zip_name = _("target/MartusClient-#{$build_number}-MacLinux.zip")
-	package :zip, :file=>zip_name
-	package(:zip).include(_("BuildFiles/Documents/installing_martus.txt"))
-	package(:zip).include(_("BuildFiles/Documents/license.txt"))
-	package(:zip).include(_("BuildFiles/Documents/README*.txt"))
-	package(:zip).include(_("martus-jar-verifier/*.bat"), :path=>'Verifier')
-	package(:zip).include(_("martus-jar-verifier/*.txt"), :path=>'Verifier')
-	package(:zip).include(_("BuildFiles/Windows/Winsock95"), :path=>"Win95")
-	package(:zip).include(artifact(BCPROV_SPEC), :path=>'LibExt')
-	package(:zip).include(artifact(INFINITEMONKEY_JAR_SPEC), :path=>'LibExt')
-	package(:zip).include(artifact(JUNIT_SPEC), :path=>'LibExt')
-	package(:zip).include(artifact(XMLRPC_SPEC), :path=>'LibExt')
-	package(:zip).include(project('martus-client').package(:sources))
+	zip(zip_name)
+	zip(zip_name).include(_("BuildFiles/Documents/installing_martus.txt"))
+	zip(zip_name).include(_("BuildFiles/Documents/license.txt"))
+	zip(zip_name).include(_("BuildFiles/Documents/README*.txt"))
+	zip(zip_name).include(_("martus-jar-verifier/*.bat"), :path=>'Verifier')
+	zip(zip_name).include(_("martus-jar-verifier/*.txt"), :path=>'Verifier')
+	zip(zip_name).include(_("BuildFiles/Windows/Winsock95"), :path=>"Win95")
+	zip(zip_name).include(artifact(BCPROV_SPEC), :path=>'LibExt')
+	zip(zip_name).include(artifact(INFINITEMONKEY_JAR_SPEC), :path=>'LibExt')
+	zip(zip_name).include(artifact(JUNIT_SPEC), :path=>'LibExt')
+	zip(zip_name).include(artifact(XMLRPC_SPEC), :path=>'LibExt')
+	zip(zip_name).include(project('martus-client').package(:sources))
 #TODO: Add docs to Mac/Linux zip
-#	package(:zip).include(docs)
+#	zip(zip_name).include(docs)
 #TODO: Add third-party licenses to Mac/Linux zip
-#	package(:zip).include(licenses)
-	package(:zip).include(artifact(LOGI_LICENSE_SPEC), :path=>'licenses/logi')
+#	zip(zip_name).include(licenses)
+	zip(zip_name).include(artifact(LOGI_LICENSE_SPEC), :path=>'licenses/logi')
 	
-
+	build(zip_name)
 end
