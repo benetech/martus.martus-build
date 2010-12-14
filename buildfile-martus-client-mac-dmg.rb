9737f97c69325bfc0077962d8b72514f8b34b7a5
kevins <kevins@localhost>
2010-12-14 21:38:23 +0000

    TT 4267: Shift a bunch of variables from the ant script to the buildr script

--- buildfile-martus-client-mac-dmg.rb
+++ buildfile-martus-client-mac-dmg.rb
@@ -4,9 +4,29 @@ define name, :layout=>create_layout_with_source_as_source(name) do
 	project.group = 'org.martus'
 	project.version = '1'
 
-    `ant -buildfile martus-client-mac-dmg.ant.xml macdmgfile`
+    buildfile_option = "-buildfile martus-client-mac-dmg.ant.xml"
+    properties = ""
+    properties << " -Dmac.app.name=Martus"
+    properties << " -Dshort.app.name=Martus"
+    properties << " -Dversion.full=3.5.1"
+    properties << " -Dversion.timestamp="
+
+    properties << " -Ddist.mactree=/home/kevins/martus-mac/" #can be temp
+    properties << " -Ddmg.dest.dir=/home/kevins/"
+    properties << " -Drawdmgfile=/home/kevins/Martus.dmg"
+    properties << " -Ddmgmount=/mounts/Martus/dmgfile"
+    properties << " -Ddmg.size.megs=10"
+
+    properties << " -Dinstaller.mac=/home/kevins/work/miradi/hg/eclipse/miradi/build/installer/Mac/" #parent of JavaApplicationStub
+    properties << " -Dapp.jar.name=martus-client-20090826.1911.jar"
+    properties << " -Dapp.jar.dir=/home/kevins/work/martus/martus/0826-client/"
+    properties << " -Dthirdparty.jars.dir=/home/kevins/work/martus/martus/martus-thirdparty/"
+    properties << " -Dvm.options=-Xmx512m"
+
+    ant = "ant #{buildfile_option} macdmgfile #{properties}"
+    `#{ant}`
     if $CHILD_STATUS != 0
-        raise "Failed in dmg ant script"
+        raise "Failed in dmg ant script #{$CHILD_STATUS}"
     end
     
 end
