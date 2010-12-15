267991e3781e23e9fc469a8de0ec94d2cce8c0a5
kevins <kevins@localhost>
2010-12-15 23:10:56 +0000

    TT 4267: use real path for official zip file

--- buildfile-martus-client-mac-dmg.rb
+++ buildfile-martus-client-mac-dmg.rb
@@ -9,7 +9,9 @@ define name, :layout=>create_layout_with_source_as_source(name) do
     hudson_job_dir = "/var/lib/hudson/jobs/martus-client-unsigned"
     dmg_mount_point = File.join(hudson_job_dir, "mounts/dmg")
     dmg_file = File.join(hudson_job_dir, "Martus.dmg")
-    production_zipfile = "/home/kevins/Download/MartusClient-#{version}-#{timestamp}-MacLinux.zip"
+    production_zipfile_dir = File.join(hudson_job_dir, "artifacts")
+    production_zip_name = "/MartusClient-#{version}-#{timestamp}-MacLinux.zip"
+    production_zipfile = File.join(production_zipfile_dir, production_zip_name)
 
     tmpdir = Dir.mktmpdir
     puts "Using temp dir: #{tmpdir}"
