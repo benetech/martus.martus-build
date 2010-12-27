58a099a6a15da3753eaf929e4bd50efea1564aef
kevins <kevins@localhost>
2010-12-27 16:24:57 +0000

    Fix build error in previous commit, calling Dir.mkdir instead of File.mkdir

--- buildfile-martus-client-mac-dmg.rb
+++ buildfile-martus-client-mac-dmg.rb
@@ -15,7 +15,7 @@ define name, :layout=>create_layout_with_source_as_source(name) do
 
     tmpdir = File.join(_('dist', 'mactree')) #was Dir.mktmpdir
     puts "Using temp dir: #{tmpdir}"
-    File.mkdir(tmpdir)
+    Dir.mkdir(tmpdir)
 
     dmg_contents_dir = File.join(tmpdir, "dmgcontents")
     raw_production_zip_contents_dir = File.join(tmpdir, "production")
