c84c1edbba8e3db6dd4ce919359e94f516d272a1
kevins <kevins@localhost>
2011-01-28 16:27:00 +0000

    Another attempt to create a DMG where the user doesn't have to copy anything

--- buildfile-martus-client-mac-dmg.rb
+++ buildfile-martus-client-mac-dmg.rb
@@ -54,7 +54,6 @@ define name, :layout=>create_layout_with_source_as_source(name) do
     properties << " -Dinstaller.mac=BuildFiles/Mac/" #parent of JavaApplicationStub
     properties << " -Dapp.dir=#{production_zip_contents_dir}"
     properties << " -Dvm.options=-Xbootclasspath/p:Contents/Resources/Java/LibExt/bc-jce.jar"
-    properties << " -DworkingDirectory=$APP_PACKAGE"
 
     properties << " -Ddist.mactree=#{dmg_contents_dir}" #can be temp
     properties << " -Ddmg.dest.dir=#{_('dist')}"
