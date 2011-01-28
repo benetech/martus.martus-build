8bf3ecd1d53247d5584aca0cd6cfe3e0f5ec5ff5
kevins <kevins@localhost>
2011-01-28 14:55:13 +0000

    Experiment with grabbing bc-jce.jar from / instead of the system Extensions folder

--- buildfile-martus-client-mac-dmg.rb
+++ buildfile-martus-client-mac-dmg.rb
@@ -58,7 +58,7 @@ define name, :layout=>create_layout_with_source_as_source(name) do
 
     properties << " -Dinstaller.mac=BuildFiles/Mac/" #parent of JavaApplicationStub
     properties << " -Dapp.dir=#{production_zip_contents_dir}"
-    properties << " -Dvm.options=-Xbootclasspath/p:/Library/Java/Extensions/bc-jce.jar"
+    properties << " -Dvm.options=-Xbootclasspath/p:/bc-jce.jar"
 
     properties << " -Ddist.mactree=#{dmg_contents_dir}" #can be temp
     properties << " -Ddmg.dest.dir=#{_('dist')}"
