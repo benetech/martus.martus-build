f6512afe6144db1f639285170870eeedee736123
kevins <kevins@localhost>
2011-10-06 04:20:31 +0000

    Change version to 3.6.1 (and pre-3.6.1)

--- buildfile-martus.rb
+++ buildfile-martus.rb
@@ -4,7 +4,7 @@ repositories.remote << 'http://download.java.net/maven/2'
 
 $build_number = ENV['BUILD_NUMBER'] || 'TEST'
 
-$client_version = '3.6'
+$client_version = '3.6.1'
 
 def build_spec(group, name, type, version)
 	return "#{group}:#{name}:#{type}:#{version}"
