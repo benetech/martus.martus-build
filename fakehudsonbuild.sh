2defcf098ec7742244892d31880529f460372040
kevins <kevins@localhost>
2011-12-23 23:10:16 +0000

    Remove -N parameter from fakehudsonbuild CVS command, to avoid creating empty BuildFile directory

--- fakehudsonbuild.sh
+++ fakehudsonbuild.sh
@@ -1,5 +1,5 @@
 mkdir /home/kevins/work/cvs/martus/temp
 cd /home/kevins/work/cvs/martus/temp
-cvs -d :ext:cvs.benetech.org:/var/local/cvs checkout -N martus
+cvs -d :ext:cvs.benetech.org:/var/local/cvs checkout martus
 cd martus
 buildr checkout build martus-client:package test=no
