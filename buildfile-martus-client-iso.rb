a6e6c8a423ce11e49531a732714b6d4c1abfebea
kevins <kevins@localhost>
2011-12-26 22:46:46 +0000

    Fix build script to not fail if iso directory already exists

--- buildfile-martus-client-iso.rb
+++ buildfile-martus-client-iso.rb
@@ -46,7 +46,7 @@ define name, :layout=>create_layout_with_source_as_source(name) do
 	
 	file iso_file => zip_file do
 		dest_dir = _(:target, 'iso')
-		Dir.mkdir(dest_dir)
+		FileUtils::mkdir_p(dest_dir)
 		unzip_file(zip_file, dest_dir)
 
 		options = '-J -r -T -hide-joliet-trans-tbl -l'
