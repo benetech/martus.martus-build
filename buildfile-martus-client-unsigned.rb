e05a0ced1c548dfdc2c817fc05d8d65c50bd1963
kevins <kevins@localhost>
2012-01-13 22:33:08 +0000

    Change extension of unsigned jar from zip to jar

--- buildfile-martus-client-unsigned.rb
+++ buildfile-martus-client-unsigned.rb
@@ -4,5 +4,5 @@ define name, :layout=>create_layout_with_source_as_source(name) do
 	project.group = 'org.martus'
 	project.version = '1'
 
-  package(:zip).merge(project('martus-client').package(:jar))
+  package(:jar).merge(project('martus-client').package(:jar))
 end
