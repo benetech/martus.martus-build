f630b4935a2999d30380b852c9f2325cdefc6b33
kevins <kevins@localhost>
2012-01-13 22:48:55 +0000

    Another attempt to create an unsigned client jar with a jar extension

--- buildfile-martus-client-unsigned.rb
+++ buildfile-martus-client-unsigned.rb
@@ -4,5 +4,13 @@ define name, :layout=>create_layout_with_source_as_source(name) do
 	project.group = 'org.martus'
 	project.version = '1'
 
-  package(:jar).merge(project('martus-client').package(:jar))
+	def package_as_signed_jar(file_name)
+	  FileTask.define_task(file_name) do
+      FileUtils.cp project('martus-client').package(:jar), file_name
+	  end
+	end
+	
+	
+	
+  package(:signed_jar)
 end
