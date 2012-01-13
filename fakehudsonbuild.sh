c056cb17917a4d1456cc23c13ba74a79d6ab9416
kevins <kevins@localhost>
2012-01-13 22:48:33 +0000

    Fake build script now also signs the jar

--- fakehudsonbuild.sh
+++ fakehudsonbuild.sh
@@ -2,4 +2,6 @@ mkdir /home/kevins/work/cvs/martus/temp
 cd /home/kevins/work/cvs/martus/temp
 cvs -d :ext:cvs.benetech.org:/var/local/cvs checkout martus
 cd martus
-buildr clean checkout martus-client-unsigned:package martus-thirdparty:package test=no
+buildr  checkout martus-client-unsigned:package martus-thirdparty:package test=no
+cd martus-client-unsigned/target
+jarsigner -keystore ~/keystore.jks -signed-jar martus-client-signed-1.jar martus-client-unsigned-1.jar SSMTSJAR
