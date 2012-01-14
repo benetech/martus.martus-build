9c633bc33ff4826c5f851d19699654f6cd3303e9
kevins <kevins@localhost>
2012-01-14 00:04:08 +0000

    Try a cleaner way to create unsigned jar

--- buildfile-martus-client-unsigned.rb
+++ buildfile-martus-client-unsigned.rb
@@ -11,7 +11,20 @@ define name, :layout=>create_layout_with_source_as_source(name) do
 	  end
 	end
 	
+#  package(:unsigned_jar)
 	
-	
-  package(:unsigned_jar)
+   package(:jar).with :manifest=>manifest.merge('Main-Class'=>'org.martus.client.swingui.Martus')
+   package(:jar).include(bcjce_sig_file)
+   package(:jar).include(bcprov_sig_file)
+ 
+   package(:jar).include(File.join(_('source', 'test', 'java'), '**/*.mlp'))
+   package(:jar).merge(project('martus-jar-verifier').package(:jar))
+   package(:jar).merge(project('martus-common').package(:jar))
+   package(:jar).merge(project('martus-utils').package(:jar))
+   package(:jar).merge(project('martus-hrdag').package(:jar))
+   package(:jar).merge(project('martus-logi').package(:jar))
+   package(:jar).merge(project('martus-swing').package(:jar))
+   package(:jar).merge(project('martus-clientside').package(:jar))
+   package(:jar).merge(project('martus-js-xml-generator').package(:jar))
+
 end
