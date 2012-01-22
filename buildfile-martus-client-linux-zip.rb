7c50be453de2fefeff2643fd87a8fcf4f7283f8c
kevins <kevins@localhost>
2012-01-22 23:30:35 +0000

    Rewrite to use buildr package method the way it was intended

--- buildfile-martus-client-linux-zip.rb
+++ buildfile-martus-client-linux-zip.rb
@@ -1,27 +1,30 @@
 name = 'martus-client-linux-zip'
 
-define name, :layout=>create_layout_with_source_as_source(name) do
+define name, :layout=>create_layout_with_source_as_source('.') do
 	project.group = 'org.martus'
 	project.version = '1'
 	
-	zip_name = _("target/MartusClient-#{$build_number}-MacLinux.zip")
-	zip(zip_name)
-	zip(zip_name).include(_("BuildFiles/Documents/installing_martus.txt"))
-	zip(zip_name).include(_("BuildFiles/Documents/license.txt"))
-	zip(zip_name).include(_("BuildFiles/Documents/README*.txt"))
-	zip(zip_name).include(_("martus-jar-verifier/*.bat"), :path=>'Verifier')
-	zip(zip_name).include(_("martus-jar-verifier/*.txt"), :path=>'Verifier')
-	zip(zip_name).include(_("BuildFiles/Windows/Winsock95"), :path=>"Win95")
-	zip(zip_name).include(artifact(BCPROV_SPEC), :path=>'LibExt')
-	zip(zip_name).include(artifact(INFINITEMONKEY_JAR_SPEC), :path=>'LibExt')
-	zip(zip_name).include(artifact(JUNIT_SPEC), :path=>'LibExt')
-	zip(zip_name).include(artifact(XMLRPC_SPEC), :path=>'LibExt')
-	zip(zip_name).include(project('martus-client').package(:sources))
-#TODO: Add docs to Mac/Linux zip
-#	zip(zip_name).include(docs)
-	include_artifacts(zip(zip_name), third_party_client_licenses, 'BuildFiles/Documents/Licenses')
-	include_artifacts(zip(zip_name), third_party_client_source, 'SourceFiles')
-	include_artifacts(zip(zip_name), third_party_client_jars, 'ThirdParty')
+	puts "Defining package for linux-zip"
+#  package(:zip, :file => _("target", "MartusClient-Linux-#{$build_number}.zip")).include(_("BuildFiles", "Documents", "installing_martus.txt"))
+	package(:zip, :file => _("target", "MartusClient-Linux-#{$build_number}.zip")).tap do | p |
+	  puts "Packaging linux zip #{_("BuildFiles")} !"
+    p.include(_("BuildFiles", "Documents", "installing_martus.txt"))
+    p.include(_("BuildFiles", "Documents", "license.txt"))
+    p.include(_("BuildFiles", "Documents", "README*.txt"))
+    p.include(_("martus-jar-verifier/*.bat"), :path=>'Verifier')
+    p.include(_("martus-jar-verifier/*.txt"), :path=>'Verifier')
+    p.include(_("BuildFiles/Windows/Winsock95"), :path=>"Win95")
+    p.include(artifact(BCPROV_SPEC), :path=>'LibExt')
+    p.include(artifact(INFINITEMONKEY_JAR_SPEC), :path=>'LibExt')
+    p.include(artifact(JUNIT_SPEC), :path=>'LibExt')
+    p.include(artifact(XMLRPC_SPEC), :path=>'LibExt')
+    p.include(project('martus-client').package(:sources))
+  #TODO: Add docs to Mac/Linux zip
+  # zip(zip_name).include(docs)
+    p.include(third_party_client_licenses, :path=>'BuildFiles/Documents/Licenses')
+    p.include(third_party_client_source, :path=>'SourceFiles')
+    p.include(third_party_client_jars, :path=>'ThirdParty')
+	end
 	
-	build(zip_name)
+	#TODO: Create SHA-1 of this file
 end
