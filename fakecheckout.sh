b6f93445f61f88f845e1820b8dac7ad5fbca41b4
kevins <kevins@localhost>
2012-01-22 22:49:30 +0000

    Separate all CVS code from build scripts, to make it easier to simulate builds on a developer workstation

---
+++ fakecheckout.sh
@@ -0,0 +1,17 @@
+rsync -r /home/kevins/work/cvs/martus/martus /home/kevins/work/cvs/martus/temp/
+
+rsync -r /home/kevins/work/cvs/martus/martus-amplifier /home/kevins/work/cvs/martus/temp/martus
+rsync -r /home/kevins/work/cvs/martus/martus-client /home/kevins/work/cvs/martus/temp/martus
+rsync -r /home/kevins/work/cvs/martus/martus-clientside /home/kevins/work/cvs/martus/temp/martus
+rsync -r /home/kevins/work/cvs/martus/martus-common /home/kevins/work/cvs/martus/temp/martus
+rsync -r /home/kevins/work/cvs/martus/martus-docs /home/kevins/work/cvs/martus/temp/martus
+rsync -r /home/kevins/work/cvs/martus/martus-hrdag /home/kevins/work/cvs/martus/temp/martus
+rsync -r /home/kevins/work/cvs/martus/martus-jar-verifier /home/kevins/work/cvs/martus/temp/martus
+rsync -r /home/kevins/work/cvs/martus/martus-js-xml-generator /home/kevins/work/cvs/martus/temp/martus
+rsync -r /home/kevins/work/cvs/martus/martus-logi /home/kevins/work/cvs/martus/temp/martus
+rsync -r /home/kevins/work/cvs/martus/martus-meta /home/kevins/work/cvs/martus/temp/martus
+rsync -r /home/kevins/work/cvs/martus/martus-mspa /home/kevins/work/cvs/martus/temp/martus
+rsync -r /home/kevins/work/cvs/martus/martus-server /home/kevins/work/cvs/martus/temp/martus
+rsync -r /home/kevins/work/cvs/martus/martus-swing /home/kevins/work/cvs/martus/temp/martus
+rsync -r /home/kevins/work/cvs/martus/martus-thirdparty /home/kevins/work/cvs/martus/temp/martus
+rsync -r /home/kevins/work/cvs/martus/martus-utils /home/kevins/work/cvs/martus/temp/martus
--- fakehudsonbuild.sh
+++ fakehudsonbuild.sh
@@ -1,7 +1,8 @@
 mkdir /home/kevins/work/cvs/martus/temp
 cd /home/kevins/work/cvs/martus/temp
-cvs -d :ext:cvs.benetech.org:/var/local/cvs checkout martus
+
 cd martus
-buildr clean checkout martus-client:package martus-thirdparty:package test=no
+buildr clean martus-client-unsigned:package martus-thirdparty:package test=no
 cd martus-client/target
-jarsigner -keystore ~/keystore.jks -signed-jar martus-client-signed-1.jar martus-client-unsigned-1.jar SSMTSJAR
+
+#jarsigner -keystore ~/keystore.jks -signed-jar martus-client-signed-1.jar martus-client-unsigned-1.jar SSMTSJAR
