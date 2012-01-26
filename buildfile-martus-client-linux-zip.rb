83a097316d4c849ba5fdbcbec227dd0a52b88c25
kevins <kevins@localhost>
2012-01-26 01:30:20 +0000

    Build a linux zip from a signed jar

--- buildfile-martus-client-linux-zip.rb
+++ buildfile-martus-client-linux-zip.rb
@@ -2,28 +2,33 @@ name = 'martus-client-linux-zip'
 
 define name, :layout=>create_layout_with_source_as_source('.') do
 	project.group = 'org.martus'
-  project.version = $BUILD_NUMBER
+  project.version = ENV['RELEASE_IDENTIFIER']
+  input_build_number = ENV['INPUT_BUILD_NUMBER']
 	
-	puts "Defining package for linux-zip"
-#  package(:zip, :file => _("target", "MartusClient-Linux-#{$BUILD_NUMBER}.zip")).include(_("BuildFiles", "Documents", "installing_martus.txt"))
-	package(:zip, :file => _("target", "MartusClient-Linux-#{$BUILD_NUMBER}.zip")).tap do | p |
-	  puts "Packaging linux zip #{_("BuildFiles")} !"
-    p.include(_("BuildFiles", "Documents", "installing_martus.txt"))
-    p.include(_("BuildFiles", "Documents", "license.txt"))
-    p.include(_("BuildFiles", "Documents", "README*.txt"))
+	puts "Defining package for linux-zip #{project.version} #{input_build_number}"
+	package(:zip, :file => _("target", "MartusClient-Linux-#{project.version}.zip")).tap do | p |
+	  signed_jar = "/var/lib/hudson/jobs/martus-client/martus-client-signed-#{input_build_number}.jar"
+	  p.include(signed_jar, :as=>"martus.jar")
+
+	  p.include(_("martus", "BuildFiles", "Documents", "installing_martus.txt"))
+    p.include(_("martus", "BuildFiles", "Documents", "license.txt"))
+    p.include(_("martus", "BuildFiles", "Documents", "README*.txt"))
+    p.include(_("martus", "BuildFiles", "Windows", "Winsock95"), :path=>"Win95")
+    
     p.include(_("martus-jar-verifier/*.bat"), :path=>'Verifier')
     p.include(_("martus-jar-verifier/*.txt"), :path=>'Verifier')
-    p.include(_("BuildFiles/Windows/Winsock95"), :path=>"Win95")
-    p.include(artifact(BCPROV_SPEC), :path=>'LibExt')
-    p.include(artifact(INFINITEMONKEY_JAR_SPEC), :path=>'LibExt')
-    p.include(artifact(JUNIT_SPEC), :path=>'LibExt')
-    p.include(artifact(XMLRPC_SPEC), :path=>'LibExt')
-    p.include(project('martus-client').package(:sources))
-  #TODO: Add docs to Mac/Linux zip
-  # zip(zip_name).include(docs)
-    p.include(third_party_client_licenses, :path=>'BuildFiles/Documents/Licenses')
-    p.include(third_party_client_source, :path=>'SourceFiles')
+    
+    #TODO: Add docs to Mac/Linux zip
+    p.include(artifact(BCPROV_SPEC), :path=>'ThirdParty')
+    p.include(artifact(INFINITEMONKEY_JAR_SPEC), :path=>'ThirdParty')
+    p.include(artifact(JUNIT_SPEC), :path=>'ThirdParty')
+    p.include(artifact(XMLRPC_SPEC), :path=>'ThirdParty')
     p.include(third_party_client_jars, :path=>'ThirdParty')
+    p.include(third_party_client_licenses, :path=>'BuildFiles/Documents/Licenses')
+
+    source_zip = _("martus-client", "target", "martus-client-sources-#{input_build_number}.zip")
+	  p.include(source_zip, :as=>"SourceFiles/martus-sources.zip")
+    p.include(third_party_client_source, :path=>'SourceFiles')
 	end
 	
 	#TODO: Create SHA-1 of this file
---
+++ fakebuildrelease.sh
@@ -0,0 +1,6 @@
+cd /home/kevins/work/cvs/martus/temp
+
+export RELEASE_IDENTIFIER=pre-4.0
+export INPUT_BUILD_NUMBER=TEST
+echo INPUT_BUILD_NUMBER=$INPUT_BUILD_NUMBER
+buildr -f martus/buildfile martus-client-linux-zip:package test=no
