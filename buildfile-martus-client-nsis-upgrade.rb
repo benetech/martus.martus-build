89acd728b50443422cd27e4bcc35433693574368
kevins <kevins@localhost>
2012-03-12 17:27:01 +0000

    Update build script to (hopefully) create a Windows upgrade installer

--- buildfile-martus-client-nsis-upgrade.rb
+++ buildfile-martus-client-nsis-upgrade.rb
@@ -2,18 +2,24 @@ name = 'martus-client-nsis-upgrade'
 
 require "#{File.dirname(__FILE__)}/buildfile-martus-client-nsis-common"
 
-define name, :layout=>create_layout_with_source_as_source(name) do
+define name, :layout=>create_layout_with_source_as_source('.') do
 	project.group = 'org.martus'
-  project.version = $BUILD_NUMBER
+  project.version = ENV['RELEASE_IDENTIFIER']
+  input_build_number = ENV['INPUT_BUILD_NUMBER']
 
-	exe_name = 'MartusSetupUpgrade.exe'
-	exe_path = _(:target, exe_name)
+  exe_name = "MartusSetupUpgrade.exe"
+  exe_path = _(:target, exe_name)
 
-	nsis_zip = create_nsis_zip_task
-	file exe_path => nsis_zip do
+  nsis_zip = create_nsis_zip_task
+  file exe_path => nsis_zip do
+    puts "Building NSIS upgrade installer"
 		run_nsis_task(nsis_zip, 'NSIS_Martus_Upgrade.nsi', exe_name)
-	end
-	
-	build(exe_path)
-end
+    destination = _(:target, "MartusClientUpgrade-#{project.version}-#{input_build_number}.exe")
+    FileUtils.mv exe_path, destination
+    create_sha_files(destination)
+  end
+  
+  build(exe_path)
 
+  artifact(MARTUSSETUP_UPGRADE_EXE_SPEC).from(exe_path)
+end
