a644123be66c0a1363cb75a3c73affd523538855
kevins <kevins@localhost>
2012-03-14 23:29:49 +0000

    Update build script to build an installer CD

--- buildfile-martus-client-nsis-cd.rb
+++ buildfile-martus-client-nsis-cd.rb
@@ -2,18 +2,25 @@ name = 'martus-client-nsis-cd'
 
 require "#{File.dirname(__FILE__)}/buildfile-martus-client-nsis-common"
 
-define name, :layout=>create_layout_with_source_as_source(name) do
+define name, :layout=>create_layout_with_source_as_source('.') do
 	project.group = 'org.martus'
-  project.version = $BUILD_NUMBER
+  project.version = ENV['RELEASE_IDENTIFIER']
+  input_build_number = ENV['INPUT_BUILD_NUMBER']
 
 	exe_name = 'MartusSetup.exe'
 	exe_path = _(:target, exe_name)
 
 	nsis_zip = create_nsis_zip_task
 	file exe_path => nsis_zip do
+    puts "Building NSIS CD installer"
 		run_nsis_task(nsis_zip, 'NSIS_Martus.nsi', exe_name)
+    destination = _(:target, "MartusCDClientSetup-#{project.version}-#{input_build_number}.exe")
+    FileUtils.mv exe_path, destination
+    create_sha_files(destination)
 	end
 	
 	build(exe_path)
+
+  artifact(MARTUSSETUP_EXE_SPEC).from(exe_path)
 end
 
--- fakebuildrelease.sh
+++ fakebuildrelease.sh
@@ -3,4 +3,7 @@ cd /home/kevins/work/cvs/martus/temp
 export RELEASE_IDENTIFIER=pre-4.0
 export INPUT_BUILD_NUMBER=TEST
 echo INPUT_BUILD_NUMBER=$INPUT_BUILD_NUMBER
-buildr -f martus/buildfile martus-client-linux-zip:package martus-client-mac-dmg:package martus-client-linux-zip:sha1 martus-client-linux-zip:sha2 martus-client-nsis-single:build martus-client-nsis-upgrade:build test=no
+buildr -f martus/buildfile test=no \
+martus-client-linux-zip:package martus-client-linux-zip:sha1 martus-client-linux-zip:sha2 \
+martus-client-nsis-single:build martus-client-iso:build martus-client-nsis-upgrade:build \
+martus-client-mac-dmg:package   
