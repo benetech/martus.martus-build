550aa15c437c0936239ec4b8b8867f56812ac064
kevins <kevins@localhost>
2012-03-15 00:01:40 +0000

    Add release build number to output files (DMG, Linux zip, nsis installers)

--- buildfile-martus-client-nsis-single.rb
+++ buildfile-martus-client-nsis-single.rb
@@ -6,6 +6,7 @@ define name, :layout=>create_layout_with_source_as_source('.') do
 	project.group = 'org.martus'
   project.version = ENV['RELEASE_IDENTIFIER']
   input_build_number = ENV['INPUT_BUILD_NUMBER']
+  release_build_number = $BUILD_NUMBER
 
 	exe_name = "MartusSetupSingle.exe"
 	exe_path = _(:target, exe_name)
@@ -14,7 +15,7 @@ define name, :layout=>create_layout_with_source_as_source('.') do
 	file exe_path => nsis_zip do
 	  puts "Building NSIS Single installer"
 		run_nsis_task(nsis_zip, 'NSIS_Martus_Single.nsi', exe_name)
-		destination = _(:target, "MartusClientSetup-#{project.version}-#{input_build_number}.exe")
+		destination = _(:target, "MartusClientSetup-#{project.version}-#{input_build_number}-#{release_build_number}.exe")
 		FileUtils.mv exe_path, destination
     create_sha_files(destination)
 	end
--- buildfile-martus-client-nsis-upgrade.rb
+++ buildfile-martus-client-nsis-upgrade.rb
@@ -6,6 +6,7 @@ define name, :layout=>create_layout_with_source_as_source('.') do
 	project.group = 'org.martus'
   project.version = ENV['RELEASE_IDENTIFIER']
   input_build_number = ENV['INPUT_BUILD_NUMBER']
+  release_build_number = $BUILD_NUMBER
 
   exe_name = "MartusSetupUpgrade.exe"
   exe_path = _(:target, exe_name)
@@ -14,7 +15,7 @@ define name, :layout=>create_layout_with_source_as_source('.') do
   file exe_path => nsis_zip do
     puts "Building NSIS upgrade installer"
 		run_nsis_task(nsis_zip, 'NSIS_Martus_Upgrade.nsi', exe_name)
-    destination = _(:target, "MartusClientUpgrade-#{project.version}-#{input_build_number}.exe")
+    destination = _(:target, "MartusClientUpgrade-#{project.version}-#{input_build_number}-#{release_build_number}.exe")
     FileUtils.mv exe_path, destination
     create_sha_files(destination)
   end
