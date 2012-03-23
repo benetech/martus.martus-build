00edca7ea515aee276696022d1b867b99efafeea
kevins <kevins@localhost>
2012-03-23 03:00:51 +0000

    Create build script to create multi-part Windows installer

--- buildfile-martus-client-nsis-single.rb
+++ buildfile-martus-client-nsis-single.rb
@@ -10,18 +10,20 @@ define name, :layout=>create_layout_with_source_as_source('.') do
 
 	exe_name = "MartusSetupSingle.exe"
 	exe_path = _(:target, exe_name)
+  destination = _(:target, "MartusClientSetup-#{project.version}-#{input_build_number}-#{release_build_number}.exe")
 
 	nsis_zip = create_nsis_zip_task
 	file exe_path => nsis_zip do
 	  puts "Building NSIS Single installer"
 		run_nsis_task(nsis_zip, 'NSIS_Martus_Single.nsi', exe_name)
-		destination = _(:target, "MartusClientSetup-#{project.version}-#{input_build_number}-#{release_build_number}.exe")
 		FileUtils.mv exe_path, destination
     create_sha_files(destination)
 	end
 	
-	build(exe_path)
-
-	artifact(MARTUSSETUP_EXE_SPEC).from(exe_path)
+	build(exe_path) do
+	  artifact(MARTUSSETUP_EXE_SPEC).from(destination)
+	  puts "Created artifact #{MARTUSSETUP_EXE_SPEC}"
+	end
+	
 end
 
