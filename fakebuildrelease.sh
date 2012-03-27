e6387ef1cba69b548b23ba3fa4aa1fd33b105ec2
kevins <kevins@localhost>
2012-03-27 17:42:12 +0000

    Create a script to run a full release build

--- fakebuildrelease.sh
+++ fakebuildrelease.sh
@@ -1,13 +1,6 @@
-cd /home/kevins/work/cvs/martus/temp
-
 export RELEASE_IDENTIFIER=pre-4.0
 export INPUT_BUILD_NUMBER=TEST
 export BUILD_NUMBER=NNN
-echo INPUT_BUILD_NUMBER=$INPUT_BUILD_NUMBER
-buildr -f martus/buildfile test=no \
-martus-client-linux-zip:package martus-client-linux-zip:sha1 martus-client-linux-zip:sha2 \
-martus-client-nsis-single:build \
-martus-client-nsis-pieces:package \
-martus-client-nsis-upgrade:build \
-martus-client-mac-dmg:package \
-martus-client-iso:build
+
+cd /home/kevins/work/cvs/martus/temp
+sh martus/buildrelease.sh
