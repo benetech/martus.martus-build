e70db5f7ce940cdced2b25e1fc6cae61a70417f8
Kevin Smith <Kevin Smith@localhost>
2012-06-28 10:16:19 -0600

    Attempt to add JFreeChart and JCommon to the build scripts

--- buildfile-martus-client.rb
+++ buildfile-martus-client.rb
@@ -62,7 +62,9 @@ define name, :layout=>create_layout_with_source_as_source(name) do
 		LAYOUTS_SPEC,
 		project('martus-jar-verifier').packages.first,
 		VELOCITY_SPEC,
-		JORTHO_SPEC
+		JORTHO_SPEC,
+		JFREECHART_SPEC,
+		JCOMMON_SPEC
 	)
 
 	build do
