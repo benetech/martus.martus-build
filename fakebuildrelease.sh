f5d9117481f1d03610a0a69c88cdc76344ca03d7
Kevin Smith <Kevin Smith@localhost>
2012-08-27 16:10:56 -0400

    Update build script so unsigned builds work on Kevin's machine again

--- fakebuildrelease.sh
+++ fakebuildrelease.sh
@@ -2,5 +2,5 @@ export RELEASE_IDENTIFIER=pre-4.0
 export INPUT_BUILD_NUMBER=TEST
 export BUILD_NUMBER=NNN
 
-cd /home/kevins/work/cvs/martus/temp
+cd /home/kevins/temp/martus/
 sh martus/buildrelease.sh
