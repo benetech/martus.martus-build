6686fa12558f543751673f9789919a17df89671b
Kevin Smith <Kevin Smith@localhost>
2012-10-04 20:03:29 -0400

    Change format of server jar naming

--- buildfile-martus-server.rb
+++ buildfile-martus-server.rb
@@ -3,7 +3,7 @@ name = "martus-server"
 define name, :layout=>create_layout_with_source_as_source(name) do
 	project.group = 'org.martus'
   project.version = $BUILD_NUMBER
-  jarpath = _(:target, "martus-server-#{today_as_iso_date}-#{project.version}.jar")
+  jarpath = _(:target, "martus-server-#{today_as_iso_date}-Build#{project.version}.jar")
 
 	compile.options.target = '1.5'
 	compile.with(
