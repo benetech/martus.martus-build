eb9a2097c80ef152d64dde2fb3f62d468bed3997
Kevin Smith <Kevin Smith@localhost>
2012-11-06 12:58:52 -0500

    Add sha files to bc-jce build scripts

--- buildfile-martus-bc-jce.rb
+++ buildfile-martus-bc-jce.rb
@@ -3,15 +3,28 @@ name = 'martus-bc-jce'
 define name, :layout=>create_layout_with_source_as_source(name) do
 	project.group = 'org.martus'
   project.version = $BUILD_NUMBER
+  date = today_without_dashes
+  jarpath = _(:target, "martus-bc-jce-#{date}.#{project.version}.jar")
 	
 	compile.options.target = '1.5'
 	compile.with(
 		BCPROV_SPEC
 	)
 
-	package :jar
+	package(:jar, :file => jarpath)
 
+  task 'sha1' => package(:jar) do
+    sha = create_server_sha1(jarpath)
+  end
+
+  task 'sha2' => package(:jar) do
+    sha = create_sha2(jarpath)
+  end
+  
+  task 'jar-with-shas' => ['sha1', 'sha2', package(:jar)]
+  
 	# TODO: Old build script signed this jar
 
-	package :sources
+	package (:sources, :file => jarpath)
+	
 end
