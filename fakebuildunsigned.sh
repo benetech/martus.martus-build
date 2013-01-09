746e663b1b9a0c0e2ca881688748de2c69643892
Kevin Smith <Kevin Smith@localhost>
2013-01-09 17:55:34 -0500

    Update comment to reflect latest reality

--- fakebuildunsigned.sh
+++ fakebuildunsigned.sh
@@ -3,7 +3,7 @@ pwd
 
 buildr --trace -f martus-build/buildfile clean martus-client:build_unsigned test=no
 
-# fake signing, when we are ready for that
+# fake signing
 pwd
 cd martus-client/target
 jarsigner -keystore ~/keystore.jks -signed-jar /var/lib/hudson/martus-client/builds/TEST/martus-client-signed-TEST.jar martus-client-unsigned-TEST.jar SSMTSJAR
