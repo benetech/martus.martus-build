f68b0337e4f8e4ff8d21919d99e0dc689aa3ed4a
Kevin Smith <Kevin Smith@localhost>
2013-01-10 10:49:12 -0500

    Fix server build errors from recent commits

--- fakebuildserver.sh
+++ fakebuildserver.sh
@@ -3,3 +3,5 @@ cd $WORKSPACE/martus
 pwd
 
 buildr --trace -f martus-build/buildfile clean martus-server:everything test=no
+echo "Ignore the RuntimeError : Error adding new sha files to hg"
+echo "--we intentionally only do that on the real server"
