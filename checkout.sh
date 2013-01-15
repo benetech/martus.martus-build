cea4be690e95af8c0527e819a4b09b16c3f2b5ad
Kevin Smith <Kevin Smith@localhost>
2013-01-15 18:59:33 -0500

    Change checkout script to check out server 4.4 (for now)

--- checkout.sh
+++ checkout.sh
@@ -1,19 +1,19 @@
 #cvs -d :ext:cvs.benetech.org:/var/local/cvs checkout martus
 cd $WORKSPACE
-hg clone ssh://mvcs/martus/martus-amplifier
-hg clone ssh://mvcs/martus/martus-bc-jce
-hg clone ssh://mvcs/martus/martus-client
-hg clone ssh://mvcs/martus/martus-clientside
-hg clone ssh://mvcs/martus/martus-common
-hg clone ssh://mvcs/martus/martus-docs
-hg clone ssh://mvcs/martus/martus-hrdag
-hg clone ssh://mvcs/martus/martus-jar-verifier
-hg clone ssh://mvcs/martus/martus-js-xml-generator
-hg clone ssh://mvcs/martus/martus-logi
-hg clone ssh://mvcs/martus/martus-meta
-hg clone ssh://mvcs/martus/martus-mspa
-hg clone ssh://mvcs/martus/martus-server
-hg clone ssh://mvcs/martus/martus-swing
-hg clone ssh://mvcs/martus/martus-thirdparty
-hg clone ssh://mvcs/martus/martus-utils
-hg clone ssh://mvcs/martus-sha
+hg clone --rev Branch_Server_4.4 ssh://mvcs/martus/martus-amplifier
+hg clone --rev Branch_Server_4.4 ssh://mvcs/martus/martus-bc-jce
+hg clone --rev Branch_Server_4.4 ssh://mvcs/martus/martus-client
+hg clone --rev Branch_Server_4.4 ssh://mvcs/martus/martus-clientside
+hg clone --rev Branch_Server_4.4 ssh://mvcs/martus/martus-common
+hg clone --rev Branch_Server_4.4 ssh://mvcs/martus/martus-docs
+hg clone --rev Branch_Server_4.4 ssh://mvcs/martus/martus-hrdag
+hg clone --rev Branch_Server_4.4 ssh://mvcs/martus/martus-jar-verifier
+hg clone --rev Branch_Server_4.4 ssh://mvcs/martus/martus-js-xml-generator
+hg clone --rev Branch_Server_4.4 ssh://mvcs/martus/martus-logi
+hg clone --rev Branch_Server_4.4 ssh://mvcs/martus/martus-meta
+hg clone --rev Branch_Server_4.4 ssh://mvcs/martus/martus-mspa
+hg clone --rev Branch_Server_4.4 ssh://mvcs/martus/martus-server
+hg clone --rev Branch_Server_4.4 ssh://mvcs/martus/martus-swing
+hg clone --rev Branch_Server_4.4 ssh://mvcs/martus/martus-thirdparty
+hg clone --rev Branch_Server_4.4 ssh://mvcs/martus/martus-utils
+hg clone --rev Branch_Server_4.4 ssh://mvcs/martus-sha
