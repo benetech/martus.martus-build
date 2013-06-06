5485d74725deb808a13d5edef7a0edee84d60e1f
Kevin Smith <Kevin Smith@localhost>
2013-06-06 11:32:19 -0700

    New orchid from Bruce; new version numbering scheme for it

--- buildfile-martus-thirdparty.rb
+++ buildfile-martus-thirdparty.rb
@@ -67,8 +67,8 @@ define name, :layout=>create_layout_with_source_as_source(name) do
 #	install artifact(ICU4J_SOURCE_SPEC).from(source_file(name, 'common/PersianCalendar', 'icu4j_3_2_license.html'))
 	install artifact(ICU4J_LICENSE_SPEC).from(license_file(name, 'common/PersianCalendar', 'icu4j_3_2_license.html'))
 	
-	install artifact(ORCHID_SPEC).from(jar_file(name, 'common/orchid', 'orchid-0.9.0.jar'))
-	install artifact(ORCHID_SOURCE_SPEC).from(source_file(name, 'common/orchid', 'orchid-src-0.9.0.zip'))
+	install artifact(ORCHID_SPEC).from(jar_file(name, 'common/orchid', 'orchid-0.9.0.9ef518c.jar'))
+	install artifact(ORCHID_SOURCE_SPEC).from(source_file(name, 'common/orchid', 'orchid-0.9.0.9ef518c-src.zip'))
 	install artifact(ORCHID_LICENSE_SPEC).from(license_file(name, 'common/orchid', 'LICENSE'))
 
 	#client
