9e91154e627f18f4988bb3a6c1d609d255739ece
Kevin Smith <Kevin Smith@localhost>
2013-09-13 20:54:14 -0700

    Change build scripts to avoid using temp; unzip jre at the proper time

--- buildfile-martus-client-nsis-common.rb
+++ buildfile-martus-client-nsis-common.rb
@@ -1,15 +1,15 @@
 def run_nsis_task(nsis_zip, nsi_name, exe_name)
-	puts "Unzipping NSIS zip..."
+	puts "Unzipping #{nsis_zip}..."
 	previous_pwd = Dir.pwd
     
     unzipped_dir = 'FilesForWindowsInstaller'
-	dest_dir = _(:temp, unzipped_dir)
+	dest_dir = _(:target, :temp, unzipped_dir)
 	FileUtils.rm_rf dest_dir
 	FileUtils.mkdir_p(dest_dir)
 	unzip_file(nsis_zip, dest_dir)
 	
 	FileUtils.chdir File.join(dest_dir, $nsis_script_dir)
-	puts "temp is #{_(:temp)}"
+	puts "dest_dir is #{dest_dir}"
 	puts "Running makensis from: #{Dir.pwd}"
 	nsis_cmd = "#{$nsis_command} #{nsi_name}"
 	puts "Running: #{nsis_cmd}"
