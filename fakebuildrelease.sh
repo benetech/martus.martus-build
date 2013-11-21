3d3fc5172ac3f3524590d6bdae5cfb4e3562d2ba
Kevin Smith <Kevin Smith@localhost>
2013-11-21 14:36:48 -0800

    TT 5267, part 1: Have fakebuildrelease do the jar signing, instead of fakebuildunsigned

--- fakebuildrelease.sh
+++ fakebuildrelease.sh
@@ -2,5 +2,11 @@ export RELEASE_IDENTIFIER=pre-4.0
 export INPUT_BUILD_NUMBER=TEST
 export BUILD_NUMBER=NNN
 
+# fake signing
+pwd
+cd martus-client/target
+jarsigner -signed-jar /var/lib/hudson/martus-client/builds/TEST/martus-client-signed-TEST.jar martus-client-unsigned-TEST.jar SSMTSJAR
+
+
 cd /home/kevins/temp/martus/
 sh martus-build/buildrelease.sh
