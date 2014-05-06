d515c67d1103b951611ba5a6a333992a0e2e355f
Kevin Smith <Kevin Smith@localhost>
2014-05-06 13:33:24 -0700

    Clean up local builds (they weren't copying the newly build jar/thirdparty to the attic)

--- fakebuildunsigned.sh
+++ fakebuildunsigned.sh
@@ -1,6 +1,9 @@
-export ATTIC_DIR="/var/lib/jenkins/martus-client/builds/$INPUT_BUILD_NUMBER/"
+export BUILD_NUMBER="TEST"
+export ATTIC_DIR="/var/lib/jenkins/martus-client/builds/$BUILD_NUMBER/"
 
 cd /home/kevins/temp/martus
 pwd
 
 buildr --trace -f martus-build/buildfile clean martus-client:build_unsigned test=no
+cp -p martus-client/target/martus-client-unsigned-$BUILD_NUMBER.jar $ATTIC_DIR
+cp -p martus-thirdparty/target/martus-thirdparty-$BUILD_NUMBER.jar $ATTIC_DIR
