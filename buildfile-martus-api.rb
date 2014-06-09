d63ab8350bf4ff33c05e1f646cf9f4aa60354b9b
charlesl <charlesl@localhost>
2014-06-09 11:14:33 -0700

    added what was in my current file in private/etc/Hosts

---
+++ buildfile-martus-api.rb
@@ -0,0 +1,30 @@
+name = 'martus-api'
+
+define name, :layout=>create_layout_with_source_as_source('.') do
+	project.group = 'org.martus'
+	project.version =$BUILD_NUMBER
+	
+	jarname = _('target', "martus-api-#{project.version}.jar")
+	package(:zip, :file=>jarname).tap do | p |
+		p.merge(project('martus-common').package).include('org/martus/common/*.class')
+		p.merge(project('martus-common').package).include('org/martus/common/bulletin/*.class')
+		p.merge(project('martus-common').package).include('org/martus/common/bulletinstore/*.class')
+		p.merge(project('martus-common').package).include('org/martus/common/crypto/*.class')
+		p.merge(project('martus-common').package).include('org/martus/common/database/*.class')
+		p.merge(project('martus-common').package).include('org/martus/common/field/*.class')
+		p.merge(project('martus-common').package).include('org/martus/common/fieldspec/*.class')
+		p.merge(project('martus-common').package).include('org/martus/common/network/*.class')
+		p.merge(project('martus-common').package).include('org/martus/common/network/mirroring/*.class')
+		p.merge(project('martus-common').package).include('org/martus/common/packet/*.class')
+		p.merge(project('martus-common').package).include('org/martus/common/utilities/*.class')
+		p.merge(project('martus-common').package).include('org/martus/common/xmlrpc/*.class')
+		p.merge(project('martus-common').package).include('org/miradi/utils/*.class')
+		p.merge(project('martus-utils').package).include('org/martus/util/*.class')
+		p.merge(project('martus-utils').package).include('org/martus/util/inputstreamwithseek/*.class')
+		p.merge(project('martus-utils').package).include('org/martus/util/language/*.class')
+		p.merge(project('martus-utils').package).include('org/martus/util/xml/*.class')
+		p.merge(project('martus-clientside').package).include('org/martus/clientside/ClientPortOverride.class')
+		p.merge(project('martus-clientside').package).include('org/martus/clientside/ClientSideNetwork*.class')
+	end
+
+end
